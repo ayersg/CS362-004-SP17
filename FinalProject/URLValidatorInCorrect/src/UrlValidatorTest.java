f418f61e3e2ea6a25e7d2d9e06a53a20f337d493
ayersg <ayersg@onid.oregonstate.edu>
2017-06-12 20:20:24 -0700

    Finished UrlValidatorTest for Final Project B

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -17,9 +17,7 @@
 
 
 import junit.framework.TestCase;
-
-
-
+import java.util.UUID;
 
 
 /**
@@ -40,29 +38,175 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
+
+    String[] validUrls = {"http://foo.com/blah_blah",
+                          "http://foo.com/blah_blah/",
+                          "http://www.example.com/wpstyle/?p=364",
+                          "https://www.example.com/foo/?bar=baz&inga=42&quux",
+                          "http://userid@example.com:8080",
+                          "http://142.42.1.1/",
+                          "ftp://foo.bar/baz",
+                          "http://a.b-c.de",
+                          "http://foo.bar/?q=Test%20URL-encoded%20stuff",
+                          "http://foo.com/blah_(wikipedia)_blah#cite-1"
+                         };
+
+    String[] invalidUrls = {"http://",
+                            "http://??",
+                            "http://3628126748",
+                            "http:// shouldfail.com",
+                            "http://-error-.invalid/",
+                            "http://userid:password@example.com:8080",                            
+                            "http://foo.bar?q=Spaces should be encoded",
+                            "foo.com",
+                            "ftps://foo.bar/"
+                          };
+
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
+     boolean validity;
+
+     for(int i=0; i<validUrls.length; i++){
+        System.out.println("\nTesting valid url: "+validUrls[i]);
+        validity = urlVal.isValid(validUrls[i]);
+        if(validity){
+          System.out.println("PASS\n");
+        }else{
+          System.out.println("FAIL\n");
+        }
+     }
+     System.out.println();
+
+    for(int i=0; i<invalidUrls.length; i++){
+        System.out.println("\nTesting invalid url: "+invalidUrls[i]);
+        validity = urlVal.isValid(invalidUrls[i]);
+        if(validity){
+            System.out.println("PASS\n");
+        }else{
+            System.out.println("FAIL\n");
+        }
+    }
+
+	   System.out.println("\nTest Finished Successfully.\n\n");
 	   
 	   
    }
+
+   // Partitions will be the prefix of a URL, a random string, and then the postfix of a URL
+   // Example:  "www." + "dalfkdfwi" + ".com"
+
+    String[] PrefixPartition = { "http://",
+                                "https://",
+                                "www.",
+                                "http://www.",
+                                "https://www.",
+                                "",
+                                "ftp://",
+                                "https://",
+                                "http://"
+                              };
+
+    String[] PostfixPartition = { ".com",
+                                  ".net",
+                                  ".org",
+                                  ".com/",
+                                  ".edu",
+                                  ".gov",
+                                  ".net/",
+                                  ".org/",
+                                  ".edu/"
+                                };
+
+    String[] PathPartition = {  "/abc",
+                                "/abc/",
+                                "/abc/def",
+                                "",
+    };
    
+   public String randomString(){
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        System.out.println("uuid = " + uuid);
+
+        return uuid;
+   }
    
    public void testYourFirstPartition()
    {
+        System.out.println("Testing the first partition: prefix\n");
+        UrlValidator url = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        String randomString = randomString();
+        int num = 9;  //current number of partitions 
+
+        for(int i = 0; i < num ; i++){
+          String randomUrl = PrefixPartition[i] + randomString + ".com";
+          System.out.println("\nURL after partition test: " + randomUrl + "\n");
+          boolean result = url.isValid(randomUrl);
+
+          if(result){
+            System.out.println(randomUrl + " URL is valid. PASS.\n");
+          } else{
+            System.out.println(randomUrl + " URL is invalid. FAIL.\n");
+          }
+
+        }
 	   
    }
    
    public void testYourSecondPartition(){
-	   
+
+        System.out.println("Testing the second partition: postfix\n");
+
+        UrlValidator url = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        String randomString = randomString();
+        int num = 9;  //current number of partitions 
+
+        for(int i = 0; i < num ; i++){
+          String randomUrl = "http://www." + randomString + PostfixPartition[i];
+          System.out.println("URL after partition test: " + randomUrl + "\n");
+          boolean result = url.isValid(randomUrl);
+
+          if(result){
+            System.out.println(randomUrl + " URL is valid. PASS.\n");
+          } else{
+            System.out.println(randomUrl + " URL is invalid. FAIL.\n");
+          }
+
+        }
    }
    
+   /*do programming based testing. Write few unit test cases. You can have some
+   sort of loop in your unit test and test different URL with each instance of
+   the loop. Something very similar to testIsValid() but your own logic and
+   idea. Even a single test will be sufficient if you write it like
+   testIsValid() method.*/
    
    public void testIsValid()
    {
+
+    String[] validScheme = {"http://", "https://", "ftp://"};
+    String[] validAuthority = {"www.example.com", "thisgood.com", "alsothis.org"};
+    String[] validPath = {"/abc", "/abc/", "/abc/def"};
+    String[] validQuery = {"?foo=bar", ""};
+
+    int bugs = 0;
+
 	   for(int i = 0;i<10000;i++)
 	   {
-		   
+		      int schemeR = (int)(Math.random()*3);
+          int authR = (int)(Math.random()*3);
+          int pathR = (int)(Math.random()*3); 
+          int queryR = (int)(Math.random()*2);
+
+          String testUrl = validScheme[schemeR] + validAuthority[authR] + validPath[pathR] + validQuery[queryR];
+          UrlValidator url = new UrlValidator();
+
+          boolean result = url.isValid(testUrl);
+
+          if(result==false){
+            bugs++;
+          }
 	   }
+
+     System.out.println("\nNumber of bugs in testIsValid: " + bugs + "\n");
    }
    
    public void testAnyOtherUnitTest()
